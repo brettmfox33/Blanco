19c2af69957b5049733fe544040332f5275ade09
bfox6 <bfox6@nd.edu>
2020-04-11 13:46:25 -0400

    Renamed modal file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import WelcomeModal from "./components/welcome/welcomeModal";
+import WelcomeModal from "./components/welcome/WelcomeModal";
 
 function App() {
   return (
