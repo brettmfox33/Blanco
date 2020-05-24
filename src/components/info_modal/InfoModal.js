ced818ac959ddfc1e5a19f494d2fbcf3db3ef5d6
bfox6 <bfox6@nd.edu>
2020-05-24 16:15:22 -0400

    Added back s;s

--- src/components/info_modal/InfoModal.js
+++ src/components/info_modal/InfoModal.js
@@ -1,35 +1,35 @@
-// /** @jsx jsx */
-// import {jsx} from "@emotion/core";
-// import Dialog from "@material-ui/core/Dialog";
-// import DialogContent from "@material-ui/core/DialogContent";
-// import Grid from "@material-ui/core/Grid";
-// import BuiltBy from "./BuiltBy";
-// import PoweredBy from "./PoweredBy";
-// import DesignedBy from "./DesignedBy";
-//
-// export default function InfoModal({modalOpen, setModalOpen}) {
-//   return (
-//     <Dialog
-//       fullWidth={true}
-//       maxWidth="sm"
-//       open={modalOpen}
-//       aria-labelledby="form-dialog-info"
-//       keepMounted
-//       onBackdropClick={() => setModalOpen(false)}
-//     >
-//       <DialogContent>
-//         <Grid
-//           container
-//           direction="column"
-//           justify="center"
-//           alignItems="center"
-//           css={{fontWeight: 'bold'}}
-//         >
-//           <BuiltBy />
-//           <PoweredBy />
-//           <DesignedBy />
-//         </Grid>
-//       </DialogContent>
-//     </Dialog>
-//   )
-// }
+/** @jsx jsx */
+import {jsx} from "@emotion/core";
+import Dialog from "@material-ui/core/Dialog";
+import DialogContent from "@material-ui/core/DialogContent";
+import Grid from "@material-ui/core/Grid";
+import BuiltBy from "./BuiltBy";
+import PoweredBy from "./PoweredBy";
+import DesignedBy from "./DesignedBy";
+
+export default function InfoModal({modalOpen, setModalOpen}) {
+  return (
+    <Dialog
+      fullWidth={true}
+      maxWidth="sm"
+      open={modalOpen}
+      aria-labelledby="form-dialog-info"
+      keepMounted
+      onBackdropClick={() => setModalOpen(false)}
+    >
+      <DialogContent>
+        <Grid
+          container
+          direction="column"
+          justify="center"
+          alignItems="center"
+          css={{fontWeight: 'bold'}}
+        >
+          <BuiltBy />
+          <PoweredBy />
+          <DesignedBy />
+        </Grid>
+      </DialogContent>
+    </Dialog>
+  )
+}
--- src/components/info_modal/PoweredBy.js
+++ src/components/info_modal/PoweredBy.js
@@ -1,84 +1,84 @@
-// /** @jsx jsx */
-// import Grid from "@material-ui/core/Grid";
-// import IconButton from "@material-ui/core/IconButton";
-// import {jsx} from "@emotion/core";
-// import {Fragment} from "react";
-//
-// export default function PoweredBy() {
-//   return (
-//     <Fragment>
-//       <Grid
-//       css={{marginTop: 10}}
-//       >
-//         Powered by
-//       </Grid>
-//       <Grid
-//         container
-//         direction="row"
-//         justify="center"
-//         alignItems="center"
-//       >
-//         <IconButton
-//           aria-label="info"
-//           href="https://reactjs.org/"
-//           target="_blank"
-//         >
-//           <img
-//             draggable={false}
-//             alt="react"
-//             src={require(`../../images/info_icons/react.png`)}
-//             css={{width: '2.1875rem'}}
-//           />
-//         </IconButton>
-//         <IconButton
-//           aria-label="info"
-//           href="https://react-redux.js.org/"
-//           target="_blank"
-//         >
-//           <img
-//             draggable={false}
-//             alt="react"
-//             src={require(`../../images/info_icons/redux.png`)}
-//             css={{width: '2.1875rem'}}
-//           />
-//         </IconButton>
-//         <IconButton
-//           aria-label="info"
-//           href="https://material-ui.com/"
-//           target="_blank"
-//         >
-//           <img
-//             draggable={false}
-//             alt="react"
-//             src={require(`../../images/info_icons/material_ui.png`)}
-//             css={{width: '2.1875rem'}}
-//           />
-//         </IconButton>
-//         <IconButton
-//           aria-label="info"
-//           href="https://www.framer.com/"
-//           target="_blank"
-//         >
-//           <img
-//             draggable={false}
-//             alt="react"
-//             src={require(`../../images/info_icons/framer.png`)}
-//             css={{width: '2.1875rem'}}
-//           />
-//         </IconButton>
-//         <IconButton
-//           aria-label="info"
-//           href="https://socket.io/"
-//           target="_blank"
-//         >
-//           <img
-//             draggable={false}
-//             alt="react"
-//             src={require(`../../images/info_icons/socket_io.png`)}
-//             css={{width: '2.1875rem'}}
-//           />
-//         </IconButton>
-//       </Grid>
-//     </Fragment>
-//   )
-// }
+/** @jsx jsx */
+import Grid from "@material-ui/core/Grid";
+import IconButton from "@material-ui/core/IconButton";
+import {jsx} from "@emotion/core";
+import {Fragment} from "react";
+
+export default function PoweredBy() {
+  return (
+    <Fragment>
+      <Grid
+      css={{marginTop: 10}}
+      >
+        Powered by
+      </Grid>
+      <Grid
+        container
+        direction="row"
+        justify="center"
+        alignItems="center"
+      >
+        <IconButton
+          aria-label="info"
+          href="https://reactjs.org/"
+          target="_blank"
+        >
+          <img
+            draggable={false}
+            alt="react"
+            src={require(`../../images/info_icons/react.png`)}
+            css={{width: '2.1875rem'}}
+          />
+        </IconButton>
+        <IconButton
+          aria-label="info"
+          href="https://react-redux.js.org/"
+          target="_blank"
+        >
+          <img
+            draggable={false}
+            alt="react"
+            src={require(`../../images/info_icons/redux.png`)}
+            css={{width: '2.1875rem'}}
+          />
+        </IconButton>
+        <IconButton
+          aria-label="info"
+          href="https://material-ui.com/"
+          target="_blank"
+        >
+          <img
+            draggable={false}
+            alt="react"
+            src={require(`../../images/info_icons/material_ui.png`)}
+            css={{width: '2.1875rem'}}
+          />
+        </IconButton>
+        <IconButton
+          aria-label="info"
+          href="https://www.framer.com/"
+          target="_blank"
+        >
+          <img
+            draggable={false}
+            alt="react"
+            src={require(`../../images/info_icons/framer.png`)}
+            css={{width: '2.1875rem'}}
+          />
+        </IconButton>
+        <IconButton
+          aria-label="info"
+          href="https://socket.io/"
+          target="_blank"
+        >
+          <img
+            draggable={false}
+            alt="react"
+            src={require(`../../images/info_icons/socket_io.png`)}
+            css={{width: '2.1875rem'}}
+          />
+        </IconButton>
+      </Grid>
+    </Fragment>
+  )
+}
